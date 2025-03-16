70f2ccf95c80ae589c598fd800db1297d4d24ea3
Denis Orlov <denisorlov1011@gmail.com>
2025-03-16 22:36:07 +0100

    Initial Commit

---
+++ Spendee/SpendeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendeeApp.swift
+//  Spendee
+//
+//  Created by Denis's MacBook on 16/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SpendeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
