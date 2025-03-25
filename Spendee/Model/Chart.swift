19c3ceb14a96f02b1f867a9883f49a7a35b49126
Denis Orlov <denisorlov1011@gmail.com>
2025-03-25 18:14:41 +0100

    [Create] - Create ChartGroup model for the StatsScreen, - Create GroupingKey to group the transactions by dates and categories and than map them to the ChartGroup

--- Spendee/Model/Chart.swift
+++ Spendee/Model/Chart.swift
@@ -4,4 +4,20 @@
 //
 //  Created by Denis's MacBook on 21/3/25.
 //
+import SwiftUI
+
+struct GroupingKey: Hashable {
+    let date: DateComponents
+    let category: String
+}
+
+struct ChartGroup: Identifiable {
+    let id: UUID = .init()
+    let date: Date
+    let category: String
+    var totalValue: Double
+    var type: TransactionType
+}
+
+
 
